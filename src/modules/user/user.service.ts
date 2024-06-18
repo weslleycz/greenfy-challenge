0e5878e93195019afaf83d33951a35ee591c671a
weslleycz <weslleysouza22@gmail.com>
2024-06-18 15:05:24 -0300

    ♻️ Refactor: Improved code readability and efficiency in the create method of the UserService class.

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,7 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { BcryptService } from '../../common/services';
-import { CreateUserDto } from './dto';
 import { UserRepository } from './user.repository';
+import { CreateUserDto, SuccessResponseDto } from './dto';
 
 @Injectable()
 export class UserService {
@@ -9,29 +9,46 @@ export class UserService {
     private readonly bcryptService: BcryptService,
     private readonly userRepository: UserRepository,
   ) {}
-  async create(data: CreateUserDto) {
-    const userExists = await this.userRepository.findOne({
-      where: {
-        email: data.email,
-      },
-    });
-    if (!userExists) {
+
+  async create(data: CreateUserDto): Promise<SuccessResponseDto> {
+    try {
+      // Verifique se o usuário já existe
+      const userExists = await this.userRepository.findOne({
+        where: {
+          email: data.email,
+        },
+      });
+
+      if (userExists) {
+        throw new HttpException(
+          'Não é possível criar uma conta porque esse e-mail já está associado a outra conta.',
+          HttpStatus.CONFLICT,
+        );
+      }
+
+      // Hash da senha
       const hashPassword = await this.bcryptService.hashPassword(data.password);
-      await this.userRepository.save({
+
+      // Criação do novo usuário
+      const newUser = {
         email: data.email,
         name: data.name,
         password: hashPassword,
         createdAt: new Date(),
         updatedAt: new Date(),
-      });
+        phoneNumber: data.phoneNumber,
+      };
+
+      await this.userRepository.save(newUser);
+
       return {
         message: 'Usuário criado com sucesso!',
         statusCode: HttpStatus.OK,
       };
-    } else {
+    } catch (error) {
       throw new HttpException(
-        'Não é possível criar uma conta porque esse e-mail já está associado a outra conta.',
-        HttpStatus.CONFLICT,
+        error.message || 'Erro ao criar o usuário.',
+        error.status || HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
   }
