974dcb560f73f6bf9612a37f7df0f0da48fec46f
weslleycz <weslleysouza22@gmail.com>
2024-06-18 01:09:19 -0300

    ✨ feat: configure TypeORM for PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DatabaseModule } from './database';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DatabaseModule, UserModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/database/ormconfig.ts
@@ -0,0 +1,25 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+dotenv.config();
+
+const config: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT as unknown as number,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  synchronize: true,
+  logging: false,
+  logger: 'file',
+  entities: [`${__dirname}../../entities/*.{ts,js}`],
+  migrations: [`${__dirname}../../migrations/*.{ts,js}`],
+  autoLoadEntities: true,
+  migrationsTableName: 'custom_migration_table',
+};
+
+export { config };
+
+export default new DataSource(config as DataSourceOptions);
